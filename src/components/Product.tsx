61c4a732191ab31e665ec87011abac9198aa662f
Saif <saif49032@gmail.com>
2023-09-21 12:56:54 +0530

    store created

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import { HiShoppingCart } from "react-icons/hi";
 import { FaHeart } from "react-icons/fa";
 import FormattedPrice from "./FormattedPrice";
-
+import { useDispatch } from "react-redux";
+import { addToCart, addToFavorite } from "@/store/nextSlice";
 const Product = ({ productData }: any) => {
+  const dispatch = useDispatch();
   return (
     <div className="w-full px-6 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6">
       {productData.map(
@@ -33,10 +35,46 @@ const Product = ({ productData }: any) => {
                 className="w-full h-full object-cover scale-90 hover:scale-100 transition-transform duration-300"
               />
               <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex flex-col translate-x-20 group-hover:translate-x-0 transition-transform duration-300">
-                <span className="w-full h-full border-b-[1px] border-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                <span
+                  onClick={() =>
+                    dispatch(
+                      addToCart({
+                        _id: _id,
+                        brand: brand,
+                        category: category,
+                        description: description,
+                        image: image,
+                        isNew: isNew,
+                        oldPrice: oldPrice,
+                        price: price,
+                        title: title,
+                        quantity: 1,
+                      })
+                    )
+                  }
+                  className="w-full h-full border-b-[1px] border-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300"
+                >
                   <HiShoppingCart />
                 </span>
-                <span className="w-full h-full sborder-b-[1px] border-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                <span
+                  onClick={() =>
+                    dispatch(
+                      addToFavorite({
+                        _id: _id,
+                        brand: brand,
+                        category: category,
+                        description: description,
+                        image: image,
+                        isNew: isNew,
+                        oldPrice: oldPrice,
+                        price: price,
+                        title: title,
+                        quantity: 1,
+                      })
+                    )
+                  }
+                  className="w-full h-full sborder-b-[1px] border-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300"
+                >
                   <FaHeart />
                 </span>
               </div>
@@ -61,7 +99,27 @@ const Product = ({ productData }: any) => {
               <p className="text-xs text-gray-600 text-justify">
                 {description.substring(0, 120)}
               </p>
-              <button className="h-10 font-medium bg-amazon_blue text-white rounded-md hover:bg-amazon_yellow hover:text-black duration-300 mt-2">Add To Cart</button>
+              <button
+                onClick={() =>
+                  dispatch(
+                    addToCart({
+                      _id: _id,
+                      brand: brand,
+                      category: category,
+                      description: description,
+                      image: image,
+                      isNew: isNew,
+                      oldPrice: oldPrice,
+                      price: price,
+                      title: title,
+                      quantity: 1,
+                    })
+                  )
+                }
+                className="h-10 font-medium bg-amazon_blue text-white rounded-md hover:bg-amazon_yellow hover:text-black duration-300 mt-2"
+              >
+                Add To Cart
+              </button>
             </div>
           </div>
         )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,8 +5,14 @@ import { BiCaretDown } from "react-icons/bi";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlLocationPin } from "react-icons/sl";
 import Link from "next/link";
+import { useSelector } from "react-redux";
+import { StateProps } from "../../../type";
 
 const Header = () => {
+  const { productData, favoriteData } = useSelector(
+    (state: StateProps) => state.next
+  );
+  console.log(productData, favoriteData);
   return (
     <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
       <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
@@ -42,9 +48,14 @@ const Header = () => {
             </span>
           </p>
         </div>
-        <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
+        <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative">
           <p>Marked</p>
           <p className="text-white font-bold">& Favorite</p>
+          {favoriteData.length > 0 && (
+            <span className="absolute right-2 top-2 w-4 h-4 border-[1px] border-gray-400 flex items-center justify-center text-xs text-amazon_yellow">
+              {favoriteData.length}
+            </span>
+          )}
         </div>
         <Link
           href={"/cart"}
@@ -57,7 +68,7 @@ const Header = () => {
           />
           <p className="text-xs text-white font-bold mt-3">Cart</p>
           <span className="absolute text-amazon_yellow text-xs top-2 left-[29px] font-semibold">
-            0
+            {productData ? productData.length : 0}
           </span>
         </Link>
       </div>
