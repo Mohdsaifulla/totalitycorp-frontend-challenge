fd00a40b1ce7ed786d21c28252b251a40191aae7
Saif <saif49032@gmail.com>
2023-09-22 10:14:57 +0530

    payment methord added

--- src/components/CartPayment.tsx
+++ src/components/CartPayment.tsx
@@ -3,10 +3,13 @@ import FormattedPrice from "./FormattedPrice";
 import { useDispatch, useSelector } from "react-redux";
 import { StateProps, StoreProduct } from "../../type";
 import { useState, useEffect } from "react";
-
+import { loadStripe } from "@stripe/stripe-js";
+import { useSession } from "next-auth/react";
 
 const CartPayment = () => {
-  const { productData,userInfo } = useSelector((state: StateProps) => state.next);
+  const { productData, userInfo } = useSelector(
+    (state: StateProps) => state.next
+  );
   const [totalAmount, setTotalAmout] = useState(0);
   useEffect(() => {
     let amt = 0;
@@ -16,6 +19,34 @@ const CartPayment = () => {
     });
     setTotalAmout(amt);
   }, [productData]);
+
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  );
+
+  const { data: session } = useSession();
+
+  const handleCheckout = async () => {
+    const stripe = await stripePromise;
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ items: productData, email: session?.user?.email }),
+    });
+    const checkoutSession=await response.json()
+
+    //Redirecting user
+
+    const result:any=await stripe?.redirectToCheckout({
+      sessionId:checkoutSession.id,
+    })
+    if(result.error){
+      alert(result?.error.message)
+    }
+    
+  };
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-2">
@@ -33,27 +64,25 @@ const CartPayment = () => {
           <FormattedPrice amount={totalAmount} />
         </span>
       </p>
-     {
-      userInfo?
-      (
+      {userInfo ? (
         <div className="flex flex-col items-center">
-        <button className="w-full h-10 text-sm font-semibold bg-amazon_blue bg-opacity-50 text-white rounded-lg hover:bg-amazon_yellow hover:text-black">
-          Proceed to buy
-        </button>
-      </div>
-      ):
-
-      (
+          <button
+            onClick={handleCheckout}
+            className="w-full h-10 text-sm font-semibold bg-amazon_blue bg-opacity-50 text-white rounded-lg hover:bg-amazon_yellow hover:text-black"
+          >
+            Proceed to buy
+          </button>
+        </div>
+      ) : (
         <div className="flex flex-col items-center">
-        <button className="w-full h-10 text-sm font-semibold bg-amazon_blue bg-opacity-50 text-white rounded-lg cursor-not-allowed">
-          Proceed to buy
-        </button>
-        <p className="text-xs mt-1 text-red-500 font-semibold animate-bounce">
-          Please login to continue
-        </p>
-      </div>
-      )
-     }
+          <button className="w-full h-10 text-sm font-semibold bg-amazon_blue bg-opacity-50 text-white rounded-lg cursor-not-allowed">
+            Proceed to buy
+          </button>
+          <p className="text-xs mt-1 text-red-500 font-semibold animate-bounce">
+            Please login to continue
+          </p>
+        </div>
+      )}
     </div>
   );
 };
